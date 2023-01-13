4bdbc7aadb744ee33d2fc541661b68e88d138d73
Marco Visaya <marco.visaya527@gmail.com>
2023-01-13 23:49:56 +0800

    Add react routes, lift reservationDates state

--- src/App.js
+++ src/App.js
@@ -1,18 +1,55 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { useState, useReducer } from "react";
 import "./App.css";
+import Main from "./components/Main";
+import Booking from "./components/Booking";
 import Nav from "./components/Nav";
 import Footer from "./components/Footer";
-import Header from "./components/Header";
-import Main from "./components/Main";
+import ConfirmedBooking from "./components/ConfirmedBooking";
+import { fetchAPI } from "./api";
+
+export const updateTimes = (state, action) => fetchAPI(new Date(action.date));
+export const initializeTimes = fetchAPI(new Date());
 
 function App() {
-  return (
-    <>
-      <Nav />
-      <Header />
-      <Main />
-      <Footer />
-    </>
-  );
+  const [availableTimes, dispatch] = useReducer(updateTimes, initializeTimes);
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+        <>
+          <Nav />
+          <Main availableTimes={availableTimes} />
+          <Footer />
+        </>
+      ),
+    },
+    {
+      path: "/booking",
+      element: (
+        <>
+          <Nav />
+          <Booking
+            availableTimes={availableTimes}
+            setAvailableTimes={dispatch}
+          />
+          <Footer />
+        </>
+      ),
+    },
+    {
+      path: "/confirmed-booking",
+      element: (
+        <>
+          <Nav />
+          <ConfirmedBooking />
+          <Footer />
+        </>
+      ),
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
