8e388cab2f2a8180f23dec47277f5fc42a8735f3
Marco Visaya <marco.visaya527@gmail.com>
2023-01-13 13:54:29 +0800

    Set semantic HTML tags

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
+import Nav from "./components/Nav";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Main from "./components/Main";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <Nav />
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
