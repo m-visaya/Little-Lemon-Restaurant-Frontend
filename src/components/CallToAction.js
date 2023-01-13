77f90dc1555765e586e71692f287a0d47a53ad05
Marco Visaya <marco.visaya527@gmail.com>
2023-01-13 23:50:58 +0800

    Rename Header to CallToAction

--- src/components/CallToAction.js
+++ src/components/CallToAction.js
@@ -1,8 +1,9 @@
 import React from "react";
 import restauranfood from "../assets/images/restauranfood.jpg";
 import Button from "./Button";
+import { Link } from "react-router-dom";
 
-export default function Header() {
+export default function CallToAction() {
   return (
     <header className="Header">
       <section>
@@ -15,7 +16,9 @@ export default function Header() {
           <br />
           recipes served with a modern twist.
         </p>
-        <Button label="Reserve a Table" classStyle="button-yellow" />
+        <Link to="booking">
+          <Button label="Reserve a Table" classStyle="button-yellow" />
+        </Link>
       </section>
       <img src={restauranfood} alt="RestaurantFood" />
     </header>
